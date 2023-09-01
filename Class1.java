cb2914b92fff1ba9b2a9121ebf69ef51f446c896
Matthew Glavosek <student49841@gmail.com>
2023-09-01 09:11:50 -0500

    Add f2 method to Class1, Class2, and HelloWorld classes

--- Class1.java
+++ Class1.java
@@ -13,8 +13,4 @@ public class Class1 {
         //v2.0
     }
 
-    private void f3(){
-        //v3.0
-    }
-
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -19,8 +19,5 @@ public class HelloWorld {
         //v2.0
     }
 
-    private void f3(){
-        //v3.0
-    }
 
 }
